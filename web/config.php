581c2ee005d97c77daa2b6b309b9e541ac41aaf1
h-inuzuka <h-inuzuka@biglobe.co.jp>
2014-11-02 17:20:06 +0900

    mysql setting

--- web/config.php
+++ web/config.php
@@ -3,12 +3,13 @@ define('TEMPLATES_DIR_PATH', __DIR__.'/templates');
 // define('DEBUG', true);
 //define('LOG_LEVEL', \Slim\Log::FATAL);
 
+/*
 $db_settings = [
     'driver' => 'sqlite',
     'database' => __DIR__.'/sqlite.db'
 //     'database' => ':memory:'
 ];
-/*
+*/
 $db_settings = [
 'driver' => 'mysql',
 'host' => '127.0.0.1',
@@ -19,4 +20,3 @@ $db_settings = [
 'charset' => 'utf8',
 'collation' => 'utf8_unicode_ci'
 ];
-*/
